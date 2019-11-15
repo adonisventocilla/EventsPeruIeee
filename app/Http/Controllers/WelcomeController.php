34659be7e3b0d012110723079c8ed584ba28edb5
Lugonis018 <lugonis@outlook.com>
2019-11-15 18:36:53 -0500

    E3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,16 +9,7 @@ use Symfony\Component\BrowserKit\Client;
 
 class WelcomeController extends Controller
 {
-   /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+   
     /**
      * Show the application dashboard.
      *
@@ -26,13 +17,14 @@ class WelcomeController extends Controller
      */
     public function index(GoutteClient $client)
     {
-        $crawler = $client->request('GET', 'https://events.vtools.ieee.org/events/search/advanced?utf8=%E2%9C%93&sub=true&store_values=true&search=&category_id=&subcategory_id=&meeting%5Bregion_spoid%5D=R9&meeting%5Bsection_spoid%5D=R90721&meeting%5Bou_spoid%5D=&society=&meeting_after=01+Sep+2019+12%3A00+AM&meeting_before=&geo_distance=&geo_search=&order=start_time&per_page=30');
-        $inlineTitleClass = 'col_title tooltip-active';
-        $crawler->filter("[class='$inlineTitleClass']")->each(function ($eventNode){
-            $dateEvent = $eventNode->filter("[href]")->first();
-            echo $dateEvent->text().'<br>';
-        });
+        // $crawler = $client->request('GET', 'https://events.vtools.ieee.org/events/search/advanced?utf8=%E2%9C%93&sub=true&store_values=true&search=&category_id=&subcategory_id=&meeting%5Bregion_spoid%5D=R9&meeting%5Bsection_spoid%5D=R90721&meeting%5Bou_spoid%5D=&society=&meeting_after=01+Sep+2019+12%3A00+AM&meeting_before=&geo_distance=&geo_search=&order=start_time&per_page=30');
+        // $inlineTitleClass = 'col_title tooltip-active';
+        // $crawler->filter("[class='$inlineTitleClass']")->each(function ($eventNode){
+        //     $dateEvent = $eventNode->filter("[href]")->first();
+        //     echo $dateEvent->text().'<br>';
+        // });
         //dd($title->html());
         //dd($crawler);
+        return view('welcome');
     }
 }
